f99aee10465746ffb5e20d68aa96057cc70f4544
Mayank Sharma <mayankece155@gmail.com>
2022-02-06 13:15:07 +0530

    Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+import { createStore } from "redux";
 
 //default state
 var defaultState = 0;
 
 //reducers
 const balanceReducer = (state = defaultState, action) => {
+  console.log("reducer invoked", action);
   switch (action.type)
   {
     case "DEPOSIT":
@@ -17,6 +19,10 @@ const balanceReducer = (state = defaultState, action) => {
       return state;
   }
 };
+
+//store
+var store = createStore(balanceReducer);
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
