0f19f2352a6c17c23f3ff854215a11ab59539099
Mayank Sharma <mayankece155@gmail.com>
2022-02-06 13:34:42 +0530

    Action Types

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 //default state
 var defaultState = 0;
 
+//action types
+const ADD_MONEY = "ADD_MONEY";
+const WITHDRAW = "WITHDRAW";
+
 //reducers
 const balanceReducer = (state = defaultState, action) => {
   console.log("reducer invoked", action);
   switch (action.type)
   {
-    case "DEPOSIT":
+    case ADD_MONEY:
       return state + action.payload.amount;
-    case "WITHDRAW":
+    case WITHDRAW:
       return state - action.payload.amount;
     default:
       return state;
@@ -30,13 +34,13 @@ store.subscribe(() => {
 //dispatch
 //dispatch: pass an action to store; it invokes the reducer automatically
 
-store.dispatch({ type: "DEPOSIT",
+store.dispatch({ type: ADD_MONEY,
 payload: { amount: 1000 }
 }); //1000
 
-store.dispatch({ type: "DEPOSIT", payload: { amount: 450 }}); //1450
+store.dispatch({ type: ADD_MONEY, payload: { amount: 450 }}); //1450
 
-store.dispatch({ type: "WITHDRAW", payload: { amount: 250 }}); //1200
+store.dispatch({ type: WITHDRAW, payload: { amount: 250 }}); //1200
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
